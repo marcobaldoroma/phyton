3386543302dca5f3ed0db28120a7b80c69ad291b
Marco Baldo <baldomarco@live.it>
2020-08-12 01:03:01 +0200

    Update SQLite.py

--- SQLite.py
+++ SQLite.py
@@ -4,10 +4,11 @@
 
 import sqlite3
 ## attento a .... R lib..... py.... conn = sqlite3.Connection('C:/Users/baldo/OneDrive/Documenti/R/win-library/3.6/BH/include/boost/python/tompkins.sqlite')
-conn = sqlite3.Connection('D:/software/Python 3.8/tompkins.sqlite')
+conn = sqlite3.Connection('C:/training/python/tompkins.sqlite')
 
 # I haven't the table parcels.. so you have to change in another folder/table. this is just to show how i can do this
 answer = conn.execute('SELECT * FROM parcels LIMIT 3')
+answer
 for row in answer:
     print (row)
 
@@ -22,7 +23,7 @@ for row in answer:
 ### to comunicate with the computer and user    
 import sqlite3
 pkey = input ('enter your parcel key: ')
-conn = sqlite3.Connection('D:/software/Python 3.8/tompkins.sqlite')
+conn = sqlite3.Connection('C:/training/python/tompkins.sqlite')
 answer = conn.execute('SELECT parcelkey, asmt FROM parcels where parcelkey = \'' + pkey + '\'') ## '\' command to see the special signs.
 for row in answer:
     print (row)
@@ -30,7 +31,7 @@ for row in answer:
 ##
 import sqlite3
 pkey = input ('enter your parcel key: ')
-conn = sqlite3.Connection('D:/software/Python 3.8/tompkins.sqlite')
+conn = sqlite3.Connection('C:/training/python/tompkins.sqlite')
 answer = conn.execute('SELECT AVG(asmt), propclass FROM parcels GROUP BY propclass')
 for row in answer:
     print (row)
@@ -38,7 +39,7 @@ for row in answer:
 
 # sum to the assessed values ordered
 #import sqlite3
-conn = sqlite3.Connection('D:/software/Python 3.8/tompkins.sqlite')
+conn = sqlite3.Connection('C:/training/python/tompkins.sqlite')
 answer = conn.execute('SELECT AVG(asmt) AS sumasmt, propclass FROM parcels GROUP BY propclass ORDER BY sumasmt')
 for row in answer:
     print (row)
